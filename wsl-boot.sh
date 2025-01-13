80545e97fe7a058a2c76d6cf81847983c47a569d
Pierre-Jiang <pierre.primary@qq.com>
2025-01-13 09:41:03 +0800

    Update: wsl-init => wsl-sandbox

--- wsl-boot.sh
+++ wsl-boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -x /usr/local/bin/wsl-sandbox.sh ]; then
+if [ -x /usr/local/wsl-sandbox/wsl-sandbox.sh ]; then
     export WSLX_ZERO_PID_FILE=/var/run/wsl-init-swapper.pid
-    exec /usr/local/bin/wsl-sandbox.sh /sbin/init
+    exec /usr/local/wsl-sandbox/wsl-sandbox.sh /sbin/init
 fi
