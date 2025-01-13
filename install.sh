ba2a173a5701ebdc54e4af3d6b70122686f7a23d
Pierre-Jiang <pierre.primary@qq.com>
2025-01-13 22:30:18 +0800

    update

--- install.sh
+++ install.sh
@@ -16,44 +16,78 @@ FMT() {
     SUC) shift && printf "${E_GRE}%s${E_PLA} " "$FMT_TYPE" ;;
     ERR) shift && printf "${E_RED}%s${E_PLA} " "$FMT_TYPE" ;;
     WRN | TIP) shift && printf "${E_YEL}%s${E_PLA} " "$FMT_TYPE" ;;
-    INF) shift && printf "${E_YEL}%s${E_PLA} " "$FMT_TYPE" ;;
+    INF) shift && printf "${E_WHI}%s${E_PLA} " "$FMT_TYPE" ;;
     esac
     printf "%s\n" "$*"
 }
 
 [ "$(id -u)" -ne 0 ] && FMT ERR "Please run as root" >&2 && exit 1
 
+eval set -- "$(getopt -o ':f' --long 'force' -- "$@" 2>/dev/null)"
+
+FORCE=0
+while true; do
+    case "$1" in
+    --) shift && break ;;
+    -f | --force) export FORCE="1" && shift ;;
+    *) shift ;;
+    esac
+done
+
 {
     FMT TIT "Install Dependencies"
 
-    if type apt-get >/dev/null 2>&1; then
-        apt-get update
-        apt-get install -y wget util-linux
-        FMT SUC "Installed wget util-linux"
-    elif type dnf >/dev/null 2>&1; then
-        dnf install -y wget util-linux
-        FMT SUC "Installed wget util-linux"
-    elif type yum >/dev/null 2>&1; then
-        yum install -y wget util-linux
-        FMT SUC "Installed wget util-linux"
-    elif type apk >/dev/null 2>&1; then
-        apk add -q wget util-linux
-        FMT SUC "Installed wget util-linux"
-    elif type zypper >/dev/null 2>&1; then
-        zypper -q install -y wget util-linux
-        FMT SUC "Installed wget util-linux"
-    elif type pacman >/dev/null 2>&1; then
-        pacman -Syu --noconfirm wget util-linux
-        FMT SUC "Installed wget util-linux"
-    else
-        MISSED_CMD=0
+    MISSED_CMD=0
+    if [ "$FORCE" -ne 1 ]; then
         for CMD in wget unshare nsenter mount umount; do
-            if ! type "$CMD" >/dev/null 2>&1; then
+            CMD_PATH=$(command -v "$CMD")
+            if [ -z "$CMD_PATH" ]; then
                 MISSED_CMD=1
-                FMT WRN "Command not found: $CMD" >&2
+                FMT TIP "Missing Command: $CMD"
+            elif [ "$(readlink -f "$CMD_PATH")" = "/bin/busybox" ]; then
+                MISSED_CMD=1
+                FMT INF "Found Command, but its busybox: $CMD"
             fi
         done
-        [ "$MISSED_CMD" -eq 1 ] && FMT ERR "Missing required dependencies" >&2 && exit 1
+    fi
+
+    if [ "$FORCE" -ne 1 ] && [ "$MISSED_CMD" -ne 1 ]; then
+        FMT INF "Not Found Missing Dependencies, Skip"
+    else
+        [ "$FORCE" -eq 1 ] || FMT INF "Missing required commands, trying to install dependencies..."
+
+        PKGS="" EXIT_CODE=0
+        if type apt-get >/dev/null 2>&1; then
+            apt-get update
+            apt-get install -y wget util-linux
+            EXIT_CODE=$?
+            PKGS="wget util-linux"
+        elif type dnf >/dev/null 2>&1; then
+            dnf install -y wget util-linux
+            EXIT_CODE=$?
+            PKGS="wget util-linux"
+        elif type yum >/dev/null 2>&1; then
+            yum install -y wget util-linux
+            EXIT_CODE=$?
+            PKGS="wget util-linux"
+        elif type apk >/dev/null 2>&1; then
+            apk add -q wget util-linux
+            EXIT_CODE=$?
+            PKGS="wget util-linux"
+        elif type zypper >/dev/null 2>&1; then
+            zypper -q install -y wget util-linux
+            EXIT_CODE=$?
+            PKGS="wget util-linux"
+        elif type pacman >/dev/null 2>&1; then
+            pacman -Syu --noconfirm wget util-linux
+            EXIT_CODE=$?
+            PKGS="wget util-linux"
+        else
+            FMT ERR "Not Found Supported Package Manager. Aborted" >&2 && exit 1
+        fi
+
+        [ "$EXIT_CODE" -ne 0 ] && FMT ERR "Failed to install dependencies" >&2 && exit 1
+        FMT SUC "Installed $PKGS"
     fi
 }
 
@@ -65,9 +99,12 @@ FMT() {
 
     BASE_URL="https://raw.githubusercontent.com/pierreown/wsl-sandbox/main"
     for SCRIPT in wsl-sandbox.sh wsl-init.sh wsl-boot.sh wsl-enter.sh; do
-        wget -q "${BASE_URL}/${SCRIPT}" -O "/usr/local/wsl-sandbox/${SCRIPT}"
-        chmod +x "/usr/local/wsl-sandbox/${SCRIPT}"
-        FMT SUC "Downloaded /usr/local/wsl-sandbox/${SCRIPT}"
+        if wget -q -t 3 -w 1 -T 5 -O "/usr/local/wsl-sandbox/${SCRIPT}" "${BASE_URL}/${SCRIPT}"; then
+            chmod +x "/usr/local/wsl-sandbox/${SCRIPT}"
+            FMT SUC "Downloaded /usr/local/wsl-sandbox/${SCRIPT}"
+        else
+            FMT ERR "Failed to download /usr/local/wsl-sandbox/${SCRIPT}" >&2
+        fi
     done
 
     for SCRIPT in wsl-sandbox.sh wsl-init.sh; do
