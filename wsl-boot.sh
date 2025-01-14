73fc453c284af0cd5510f5d128e1385d811328ed
Pierre-Jiang <pierre.primary@qq.com>
2025-01-15 07:54:41 +0800

    update

--- wsl-boot.sh
+++ wsl-boot.sh
@@ -3,4 +3,4 @@
 SBOX_ZERO_PID_FILE="/var/run/wsl-init-sandbox.pid"
 
 echo "$$" >"$SBOX_ZERO_PID_FILE"
-exec unshare --fork --setgroups allow --pid --mount-proc -- /sbin/init
+exec unshare --fork --pid --mount-proc -- /sbin/init
