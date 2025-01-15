ecfe5c084be1c83bc132cf4dc39e7a1317225833
Pierre-Jiang <pierre.primary@qq.com>
2025-01-15 10:20:39 +0800

    update

--- wsl-boot.sh
+++ wsl-boot.sh
@@ -1,6 +1,45 @@
 #!/bin/sh
 
-SBOX_ZERO_PID_FILE="/var/run/wsl-init-sandbox.pid"
+set -e
 
-echo "$$" >"$SBOX_ZERO_PID_FILE"
-exec unshare --fork --pid --mount-proc -- /sbin/init
+INIT_PID_FILE="/var/run/sbox-wsl-init.pid"
+
+# Clear the init PID file
+: >"$INIT_PID_FILE" #
+
+# Create a named pipe (FIFO)
+PIPE="/var/run/sandbox-wsl-init.pipe"
+[ ! -p "$PIPE" ] || rm -f "$PIPE"
+mkfifo "$PIPE" 2>/dev/null
+
+# Start unshare with a new PID and mount namespace, and run /sbin/init
+unshare --fork --pid --mount-proc -- sh -c "echo 'ok' >'$PIPE'; exec /sbin/init;" &
+BOOT_PID=$!
+
+# Wait for the child process to notify the parent
+read -r _ <"$PIPE"
+rm -f "$PIPE"
+
+# Check if the children file exists
+CHILDS_FILE="/proc/${BOOT_PID}/task/${BOOT_PID}/children"
+[ -f "$CHILDS_FILE" ] || exit 1
+
+# Read the children PIDs
+set -- "$(cat "$CHILDS_FILE")"
+
+# Find the init process
+INIT_PID="$1"
+for CHILD_PID in "$@"; do
+    [ -f "/proc/${CHILD_PID}/comm" ] || continue
+    read -r COMM <"/proc/${CHILD_PID}/comm"
+    [ "${COMM##*/}" = 'init' ] || continue
+    INIT_PID="$CHILD_PID"
+    break
+done
+[ -n "$INIT_PID" ] || exit 1
+
+# Write the init PID to the file
+echo "$INIT_PID" >"$INIT_PID_FILE"
+
+# Print the init PID
+echo "$INIT_PID"
--- wsl-enter.sh
+++ wsl-enter.sh
@@ -1,83 +1,30 @@
 #!/bin/sh
 
-# custom pid file
-SBOX_ZERO_PID_FILE="/var/run/wsl-init-sandbox.pid"
+INIT_PID_FILE="/var/run/sbox-wsl-init.pid"
+INIT_PID=""
 
-wait_for_init() {
-    _TIMES=0
+# Retry up to 20 times to get init PID
+RETRY_TIMES=0
+while [ $RETRY_TIMES -lt 20 ]; do
+    [ -f "$INIT_PID_FILE" ] && read -r INIT_PID <"$INIT_PID_FILE" 2>/dev/null
+    [ -n "$INIT_PID" ] && break
+    sleep 0.5
+    RETRY_TIMES=$((RETRY_TIMES + 1))
+done
+[ -n "$INIT_PID" ] || return
 
-    # maximum 20 times of attempts
-    while [ $_TIMES -lt 20 ]; do
-        _ZERO_PID="" _ZERO_PID_FILE="${SBOX_ZERO_PID_FILE}"
-        _INIT_PID="" _INIT_PID_FILE=""
-        _INIT_CMD="" _INIT_CMD_FILE=""
+# Exit if process doesn't exist
+[ -e "/proc/$INIT_PID/exe" ] || return
 
-        # find zero pid
-        [ -n "$_ZERO_PID_FILE" ] && [ -f "$_ZERO_PID_FILE" ] && read -r _ZERO_PID _READ_ <"$_ZERO_PID_FILE" 2>/dev/null
-        [ -n "$_ZERO_PID" ] && _INIT_PID_FILE="/proc/${_ZERO_PID}/task/${_ZERO_PID}/children"
+# Default to shell if no command provided
+[ $# -gt 0 ] || set -- "${SHELL:-/bin/sh}"
 
-        while true; do
-            # find init process pid
-            [ -n "$_INIT_PID_FILE" ] && [ -f "$_INIT_PID_FILE" ] && read -r _INIT_PID _READ_ <"$_INIT_PID_FILE" 2>/dev/null
-            [ -n "$_INIT_PID" ] && _INIT_CMD_FILE="/proc/${_INIT_PID}/comm"
+# Default to current working directory
+WORK_DIR="$(pwd)" && : "${WORK_DIR:=/}"
 
-            # find init process command
-            [ -n "$_INIT_CMD_FILE" ] && [ -f "$_INIT_CMD_FILE" ] && read -r _INIT_CMD _READ_ <"$_INIT_CMD_FILE" 2>/dev/null
-
-            # process is unshare, then get its children
-            [ "${_INIT_CMD##*/}" = 'unshare' ] && _INIT_PID_FILE="/proc/${_INIT_PID}/task/${_INIT_PID}/children" && continue
-
-            # process is not unshare, break
-            break
-        done
-
-        # found init process, break
-        if [ -n "$_INIT_PID" ]; then
-            SBOX_INIT_PID="${_INIT_PID}"
-            break
-        fi
-
-        # not found init process, wait and retry
-        sleep .5
-        _TIMES=$((_TIMES + 1))
-    done
-}
-
-enter_sandbox() {
-    # wait for init process
-    wait_for_init
-
-    _INIT_PID="${SBOX_INIT_PID}"
-
-    # cannot get init process, exit
-    [ -n "$_INIT_PID" ] || return
-
-    _WORK_DIR="$(pwd)"
-
-    # export variables for child process
-    export SBOX_ENV_WORK_DIR="${_WORK_DIR}"
-
-    # default exec shell
-    [ $# -gt 0 ] || set -- "${SHELL:-/bin/sh}"
-
-    # enter its namespace and exec shell
-    # not use '--wdns' flag, because work directory may be not exist in namespace
-    # shellcheck disable=SC2016
-    exec nsenter --all --preserve-credentials --target "${_INIT_PID}" -- sh -c '
-        _WORK_DIR="${SBOX_ENV_WORK_DIR}"
-
-        # change work directory
-        if [ -d "${_WORK_DIR:="/"}" ]; then
-            cd "${_WORK_DIR}" 2>/dev/null || true
-        fi
-
-        # cleanup environment
-        unset OLDPWD
-        unset SBOX_ENV_WORK_DIR
-
-        # execute
-        exec "$@"
-    ' -- "$@"
-}
-
-enter_sandbox "$@"
+# Enter the namespace of the init process
+exec nsenter --all --preserve-credentials --target "$INIT_PID" -- sh -c "
+    [ -d '${WORK_DIR}' ] && cd '${WORK_DIR}' || true
+    unset OLDPWD
+    exec \"\$@\"
+" -- "$@"
